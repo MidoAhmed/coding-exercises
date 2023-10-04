42ba4223dd61044e5d916ab2c7fb222ac9f4c86a
med <med.elkhalki0@gmail.com>
2023-10-04 08:41:40 +0200

    add tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -222,4 +222,240 @@ describe("distributeBoxesEvenly", () => {
 
     expect(result).toEqual(expected);
   });
+
+  it(" offset", () => {
+    let clawPos = 4;
+    const boxes = [0, 2, 1, 0, 0, 1];
+    let boxInClaw = false;
+
+    const result = [];
+    const expectedBoxes = [1, 1, 1, 1, 0, 0];
+
+    while (
+      boxes.some((stackBoxes, index) => stackBoxes !== expectedBoxes[index]) &&
+      result.length < 200
+    ) {
+      const action = solve(clawPos, boxes, boxInClaw);
+
+      if (!["PICK", "RIGHT", "LEFT", "PLACE"].includes(action)) {
+        break;
+      } else if (action === "PICK") {
+        boxInClaw = true;
+        boxes[clawPos] -= 1;
+      } else if (action === "PLACE") {
+        boxInClaw = false;
+        boxes[clawPos] += 1;
+      } else if (action === "RIGHT") {
+        clawPos += 1;
+      } else if (action === "LEFT") {
+        clawPos -= 1;
+      }
+
+      result.push(action);
+    }
+
+    const expected = [
+      "LEFT",
+      "LEFT",
+      "LEFT",
+      "PICK",
+      "LEFT",
+      "PLACE",
+      "RIGHT",
+      "RIGHT",
+      "RIGHT",
+      "RIGHT",
+      "RIGHT",
+      "PICK",
+      "LEFT",
+      "LEFT",
+      "PLACE",
+    ];
+
+    expect(result).toEqual(expected);
+  });
+
+  it("solo box", () => {
+    let clawPos = 1;
+    const boxes = [0, 0, 0, 0, 0, 1];
+    let boxInClaw = false;
+
+    const result = [];
+    const expectedBoxes = [1, 0, 0, 0, 0, 0];
+
+    while (
+      boxes.some((stackBoxes, index) => stackBoxes !== expectedBoxes[index]) &&
+      result.length < 200
+    ) {
+      const action = solve(clawPos, boxes, boxInClaw);
+
+      if (!["PICK", "RIGHT", "LEFT", "PLACE"].includes(action)) {
+        break;
+      } else if (action === "PICK") {
+        boxInClaw = true;
+        boxes[clawPos] -= 1;
+      } else if (action === "PLACE") {
+        boxInClaw = false;
+        boxes[clawPos] += 1;
+      } else if (action === "RIGHT") {
+        clawPos += 1;
+      } else if (action === "LEFT") {
+        clawPos -= 1;
+      }
+
+      result.push(action);
+    }
+
+    const expected = [
+      "RIGHT",
+      "RIGHT",
+      "RIGHT",
+      "RIGHT",
+      "PICK",
+      "LEFT",
+      "LEFT",
+      "LEFT",
+      "LEFT",
+      "LEFT",
+      "PLACE",
+    ];
+
+    expect(result).toEqual(expected);
+  });
+
+  it("should return  ['PICK', 'LEFT', 'PLACE'] ", () => {
+    let clawPos = 2;
+    const boxes = [1, 0, 1];
+    let boxInClaw = false;
+
+    const result = [];
+    const expectedBoxes = [1, 1, 0];
+
+    while (
+      boxes.some((stackBoxes, index) => stackBoxes !== expectedBoxes[index]) &&
+      result.length < 200
+    ) {
+      const action = solve(clawPos, boxes, boxInClaw);
+
+      if (!["PICK", "RIGHT", "LEFT", "PLACE"].includes(action)) {
+        break;
+      } else if (action === "PICK") {
+        boxInClaw = true;
+        boxes[clawPos] -= 1;
+      } else if (action === "PLACE") {
+        boxInClaw = false;
+        boxes[clawPos] += 1;
+      } else if (action === "RIGHT") {
+        clawPos += 1;
+      } else if (action === "LEFT") {
+        clawPos -= 1;
+      }
+
+      result.push(action);
+    }
+
+    const expected = ["PICK", "LEFT", "PLACE"];
+
+    expect(result).toEqual(expected);
+  });
+
+  it("should return  ['RIGHT', 'PICK', 'LEFT', 'PLACE'] ", () => {
+    let clawPos = 0;
+    const boxes = [2, 3];
+    let boxInClaw = false;
+
+    const result = [];
+    const expectedBoxes = [3, 2];
+
+    while (
+      boxes.some((stackBoxes, index) => stackBoxes !== expectedBoxes[index]) &&
+      result.length < 200
+    ) {
+      const action = solve(clawPos, boxes, boxInClaw);
+
+      if (!["PICK", "RIGHT", "LEFT", "PLACE"].includes(action)) {
+        break;
+      } else if (action === "PICK") {
+        boxInClaw = true;
+        boxes[clawPos] -= 1;
+      } else if (action === "PLACE") {
+        boxInClaw = false;
+        boxes[clawPos] += 1;
+      } else if (action === "RIGHT") {
+        clawPos += 1;
+      } else if (action === "LEFT") {
+        clawPos -= 1;
+      }
+
+      result.push(action);
+    }
+
+    const expected = ["RIGHT", "PICK", "LEFT", "PLACE"];
+
+    expect(result).toEqual(expected);
+  });
+
+  it("should return  [ 'RIGHT', 'RIGHT', 'RIGHT', 'PICK', 'LEFT',  'PLACE', 'RIGHT', 'PICK', 'LEFT',  'LEFT',  'PLACE', 'RIGHT', 'RIGHT', 'PICK',  'LEFT',  'LEFT', 'LEFT',  'PLACE', 'RIGHT', 'RIGHT', 'RIGHT', 'PICK',  'LEFT',  'LEFT', 'LEFT',  'LEFT',  'PLACE' ] ", () => {
+    let clawPos = 2;
+    const boxes = [2, 0, 0, 0, 0, 5];
+    let boxInClaw = false;
+
+    const result = [];
+    const expectedBoxes = [2, 1, 1, 1, 1, 1];
+
+    while (
+      boxes.some((stackBoxes, index) => stackBoxes !== expectedBoxes[index]) &&
+      result.length < 200
+    ) {
+      const action = solve(clawPos, boxes, boxInClaw);
+
+      if (!["PICK", "RIGHT", "LEFT", "PLACE"].includes(action)) {
+        break;
+      } else if (action === "PICK") {
+        boxInClaw = true;
+        boxes[clawPos] -= 1;
+      } else if (action === "PLACE") {
+        boxInClaw = false;
+        boxes[clawPos] += 1;
+      } else if (action === "RIGHT") {
+        clawPos += 1;
+      } else if (action === "LEFT") {
+        clawPos -= 1;
+      }
+
+      result.push(action);
+    }
+
+    const expected = [
+      "RIGHT",
+      "RIGHT",
+      "RIGHT",
+      "PICK",
+      "LEFT",
+      "PLACE",
+      "RIGHT",
+      "PICK",
+      "LEFT",
+      "LEFT",
+      "PLACE",
+      "RIGHT",
+      "RIGHT",
+      "PICK",
+      "LEFT",
+      "LEFT",
+      "LEFT",
+      "PLACE",
+      "RIGHT",
+      "RIGHT",
+      "RIGHT",
+      "PICK",
+      "LEFT",
+      "LEFT",
+      "LEFT",
+      "LEFT",
+      "PLACE",
+    ];
+    console.debug(result);
+    expect(result).toEqual(expected);
+  });
 });
